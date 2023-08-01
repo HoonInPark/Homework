2976b1522f8fda611e3a9882907900960e1ed4c3
hooninpark <hooninpark@gmail.com>
2023-08-01 16:50:20 +0900

    solve factorial problem with recursive func

---
+++ _001_solution/_007_solution/Advanced_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+// 5 => (5 - 0) * (5 - 1) * (5 - 2) * (5 - 3) * (5 - 4)
+int factorial(int _num) {
+	if (_num == 0 || _num ==1)
+	{
+		return 1;
+	}
+	return _num * factorial(_num - 1);
+}
+
+void main() {
+	int num;
+	printf("정수를 입력하세요 >> ");
+	scanf_s("%d", &num);
+
+	printf("%d", factorial(num));
+}
