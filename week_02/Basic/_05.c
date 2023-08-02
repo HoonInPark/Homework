266689e05ea543b43befa2df807686dda84ee56b
mbbmbb <112379701+mbbmbb@users.noreply.github.com>
2023-08-02 12:31:25 +0900

    Create _05.c

---
+++ week_02/Basic/_05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 0, i = 0;
+
+	printf("[양의 정수를 입력하세요.]\n -> ");
+	scanf_s("%d", &num);
+
+	if (num > 0)
+	{
+		do
+		{
+			printf("[감사합니다.]\n");
+			i++;
+		} while (i < num);
+	}
+	else
+	{
+		printf("[종료되었습니다.]\n");
+	}
+}
