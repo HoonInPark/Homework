f736ded88b18ec7c015c5b9575ce9a8a23c8a76b
mbbmbb <112379701+mbbmbb@users.noreply.github.com>
2023-08-02 12:32:29 +0900

    Create _07.c

---
+++ week_02/Basic/_07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 1, num = 0;
+
+	printf("[출력할 구구단을 입력하세요.]\n -> ");
+	scanf_s("%d", &num);
+
+	do
+	{
+		printf("[%d * %d = %d]\n", num, i, num * i);
+		i++;
+	} while (i < 10);
+}
