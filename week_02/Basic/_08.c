574b365d37cf7ead7bb9705e129c3e6d5d7711aa
mbbmbb <112379701+mbbmbb@users.noreply.github.com>
2023-08-02 12:33:00 +0900

    Create _08.c

---
+++ week_02/Basic/_08.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 0, total = 0;
+
+	do
+	{
+		printf("[정수를 입력하세요. (0 : 종료)]\n -> ");
+		scanf_s("%d", &num);
+
+		total += num;
+
+	} while (num != 0);
+	printf("[총합 : %d]\n", total);
+}
