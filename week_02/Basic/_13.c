f2d19ec441794c1637c0481e5c0650c010ed3ab0
mbbmbb <112379701+mbbmbb@users.noreply.github.com>
2023-08-02 14:42:30 +0900

    Create _13.c

---
+++ week_02/Basic/_13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 0;
+
+	printf("[양의 정수를 입력하세요.]\n -> ");
+	scanf_s("%d", &num);
+
+	if (num > 0)
+	{
+		for (int i = 0; i < num; i++)
+		{
+			printf("[감사합니다.]\n");
+		}
+	}
+}
