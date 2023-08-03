9a7af6174e16de8bd67b29f45e3219a7677c5fdb
mbbmbb <112379701+mbbmbb@users.noreply.github.com>
2023-08-03 13:59:13 +0900

    Update _07.c

--- week_02/Middle/_07.c
+++ week_02/Middle/_07.c
@@ -4,18 +4,19 @@
 int Absolutevalue(int num01, int num02)
 {
 	int value = abs(num01 - num02);
-	printf("[절댓값 : %d]\n", value);
 
+	return value;
 }
 
 int main()
 {
-	int num01 = 0, num02 = 0;
+	int num01 = 0, num02 = 0, value = 0;
 
 	printf("[정수 2개를 입력하세요.]\n -> ");
 	scanf_s("%d %d", &num01, &num02);
 
-	Absolutevalue(num01, num02);
-
+	value = Absolutevalue(num01, num02);
+	printf("[절댓값 : %d]\n", value);
+	
 	return 0;
 }
