057992b978e821980cfa99d4d58aa1335d311e5d
hooninpark <hooninpark@gmail.com>
2023-08-05 06:44:13 +0900

    strike ball logic

--- _001_solution/_007_solution/Advanced_6.c
+++ _001_solution/_007_solution/Advanced_6.c
@@ -14,14 +14,20 @@ int getUserInput() {
 	return num;
 }
 
-int countBall(int _userInput[NUM_COMPARE]) {
+void countBall(int _userInput[NUM_COMPARE]) {
 	int strike = 0;
 	int ball = 0;
 
-	// 사용자 입력과 컴퓨터의 난수를 (생성과 동시에) 비교
-	
+	// 사용자 입력과 컴퓨터의 난수를 (생성과 동시에) 비교 1. 동일한 수가 있냐? 2. 그 수와 인덱스가 일치하냐?
+	for (int i = 0; i < NUM_COMPARE; i++)
+	{
+		int randNum_temp = makeRandNum();
 
-	return 0;
+		for (int j = 0; j < NUM_COMPARE; j++) {
+			if (randNum_temp == _userInput[j] && i == j) {	strike++; }
+			else if (randNum_temp == _userInput[j] && i != j) {	ball++;	}
+		}
+	}
 }
 
 void main() {
