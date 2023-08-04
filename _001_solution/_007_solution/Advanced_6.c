042ec43bf2a92318a8d33e2e3b1f837dc03e4a43
hooninpark <hooninpark@gmail.com>
2023-08-04 23:19:15 +0900

    comparing int logic is working...

--- _001_solution/_007_solution/Advanced_6.c
+++ _001_solution/_007_solution/Advanced_6.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_COMPARE 3
+
 int makeRandNum() {
 	return rand() % 10;
 }
@@ -12,11 +14,23 @@ int getUserInput() {
 	return num;
 }
 
-int countBall(int num0, int num1, int num2) { 
-	int strike;
-	int ball;
+int countBall(int _userInput[NUM_COMPARE]) {
+	int computerInput[NUM_COMPARE];
 
-	if (!strike || !ball)
+	int strike = 0;
+	int ball = 0;
+
+	for (int i = 0; i < NUM_COMPARE; i++)
+		computerInput[i] = makeRandNum();
+
+	// 사용자 입력과 컴퓨터의 난수를 비교
+	
+
+	if (!(strike * ball)) // 둘 중 하나가 0이면 0이 아닌 녀석을 리턴하려고 노력하되, 둘 다 0이면 그냥 0 리턴.
+	{
+
+	}
+	else
 	{
 
 	}
@@ -25,5 +39,9 @@ int countBall(int num0, int num1, int num2) {
 }
 
 void main() {
+	int userInput[NUM_COMPARE];
+	for (int i = 0; i < NUM_COMPARE; i++)
+		userInput[i] = getUserInput();
+
 
 }
