78b8d4f88fe64fe529acba29c75aaad86c1f53c7
hooninpark <hooninpark@gmail.com>
2023-08-04 23:37:12 +0900

    comparing logic fixing

--- _001_solution/_007_solution/Advanced_6.c
+++ _001_solution/_007_solution/Advanced_6.c
@@ -4,7 +4,7 @@
 #define NUM_COMPARE 3
 
 int makeRandNum() {
-	return rand() % 10;
+	return rand() % 10; // 나머지가 1~9 무작위. 
 }
 
 int getUserInput() {
@@ -15,31 +15,18 @@ int getUserInput() {
 }
 
 int countBall(int _userInput[NUM_COMPARE]) {
-	int computerInput[NUM_COMPARE];
-
 	int strike = 0;
 	int ball = 0;
 
-	for (int i = 0; i < NUM_COMPARE; i++)
-		computerInput[i] = makeRandNum();
-
-	// 사용자 입력과 컴퓨터의 난수를 비교
+	// 사용자 입력과 컴퓨터의 난수를 (생성과 동시에) 비교
 	
-
-	if (!(strike * ball)) // 둘 중 하나가 0이면 0이 아닌 녀석을 리턴하려고 노력하되, 둘 다 0이면 그냥 0 리턴.
-	{
-
-	}
-	else
-	{
-
-	}
 
 	return 0;
 }
 
 void main() {
 	int userInput[NUM_COMPARE];
+
 	for (int i = 0; i < NUM_COMPARE; i++)
 		userInput[i] = getUserInput();
 
