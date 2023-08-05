11183a1364585c7bc942c78cbfe98c983c823ed1
hooninpark <hooninpark@gmail.com>
2023-08-05 11:33:32 +0900

    for 3rd lec

--- _001_solution/_007_solution/Advanced_6.c
+++ _001_solution/_007_solution/Advanced_6.c
@@ -5,7 +5,7 @@
 #define NUM_COMPARE 3
 
 int makeRandNum() {
-	return rand() % 10; // 나머지가 1~9 무작위.	
+	return rand() % 10; // 나머지가 0~9 무작위.
 }
 
 int getUserInput() {
@@ -24,40 +24,68 @@ void showResult(int _result, int _userInput[NUM_COMPARE]) {
 		printf("\n");
 		for (int i = 0; i < NUM_COMPARE; i++)
 			printf("%d ", _userInput[i]);
-	} 
+		printf("\n");
+		callFuncCount = 0;
+	}
 }
 
 void countBall(int _userInput[NUM_COMPARE]) {
 	int strike = 0;
 	int ball = 0;
+
 	srand(time(NULL));
+	int randNum_Arr[NUM_COMPARE];
 
-	// 사용자 입력과 컴퓨터의 난수를 (생성과 동시에) 비교 1. 동일한 수가 있냐? 2. 그 수와 인덱스가 일치하냐?
-	for (int i = 0; i < NUM_COMPARE; i++)
-	{
-		int randNum_temp = makeRandNum();
-
-		for (int j = 0; j < NUM_COMPARE; j++) {
-			if (randNum_temp == _userInput[j] && i == j) 
-				strike++; 
-			else if (randNum_temp == _userInput[j] && i != j) 
-				ball++;	
-		}
-
-		showResult(randNum_temp, _userInput);
+	// 중복이 없는 무작위 수를 생성
+	for (int i = 0; i < NUM_COMPARE; i++) {
+		int num;
+		int isDuplicate;
+		do {
+			num = makeRandNum();
+			isDuplicate = 0; // 기본값으로 초기화
+			for (int j = 0; j < i; j++) {
+				if (randNum_Arr[j] == num) {
+					isDuplicate = 1;
+					break;
+				}
+			}
+		} while (isDuplicate);
+		randNum_Arr[i] = num;
 	}
 
-	if (strike < 3) 
-		printf("%d strike %d ball", strike, ball); 
-	else 
-		printf("%d strike out!!", strike); 
+	for (int i = 0; i < NUM_COMPARE; i++) {
+		for (int j = 0; j < NUM_COMPARE; j++) {
+			if (randNum_Arr[i] == _userInput[j] && i == j) {
+				strike++;
+			}
+			else if (randNum_Arr[i] == _userInput[j] && i != j) {
+				ball++;
+			}
+		}
+
+		showResult(randNum_Arr[i], _userInput);
+	}
+
+	if (strike < 3)
+		printf("%d strike %d ball\n", strike, ball);
+	else
+		printf("%d strike out!!\n", strike);
 }
 
 void main() {
 	int userInput[NUM_COMPARE];
+	int num_continue;
+	printf("숫자 야구 게임을 시작합니다.\n");
 
-	for (int i = 0; i < NUM_COMPARE; i++)
-		userInput[i] = getUserInput();
+	do {
+		for (int i = 0; i < NUM_COMPARE; i++)
+			userInput[i] = getUserInput();
 
-	countBall(userInput);
+		countBall(userInput);
+
+		printf("게임을 다시 시작하시겠습니까? (1: 재시작, 0: 종료) >> ");
+		scanf_s("%d", &num_continue);
+	} while (num_continue);
+
+	printf("게임을 종료합니다.\n");
 }
